7def4d372b19f0fea66921da30e653d77095df8d
Usman <farooqiu@uwindsor.ca>
2023-02-21 16:22:09 -0500

    Created Android Studio Project

---
+++ app/src/main/java/com/example/ultimate_sweat_buddies/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ultimate_sweat_buddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
