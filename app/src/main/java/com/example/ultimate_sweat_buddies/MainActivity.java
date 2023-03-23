8e2674fe7c3db7e407524d4a6aa554b9d76f5afe
Brady-Malott <55215446+Brady-Malott@users.noreply.github.com>
2023-03-22 20:42:17 -0400

    Added login activity and set dark theme

--- app/src/main/java/com/example/ultimate_sweat_buddies/MainActivity.java
+++ app/src/main/java/com/example/ultimate_sweat_buddies/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ultimate_sweat_buddies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.ultimate_sweat_buddies.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
