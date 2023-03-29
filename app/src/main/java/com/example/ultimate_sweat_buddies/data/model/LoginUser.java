b4bf1010dac9a3da74dba0965a506abc0c37b9aa
Usman <farooqiu@uwindsor.ca>
2023-03-29 18:49:31 -0400

    implemented store user

---
+++ app/src/main/java/com/example/ultimate_sweat_buddies/data/model/LoginUser.java
@@ -0,0 +1,26 @@
+package com.example.ultimate_sweat_buddies.data.model;
+
+public class LoginUser {
+    private String userName, userEmail;
+
+    public LoginUser(){
+        this.userName = null;
+        this.userEmail = null;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
